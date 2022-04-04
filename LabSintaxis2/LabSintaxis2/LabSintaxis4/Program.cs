7711ba3e7794382250edd3d3602c54c8ae4ea8a6
Bautista Guerra <bauguerra@gmail.com>
2022-04-04 20:25:46 -0300

    Subo tp2 y lab hecho en clase 4/4/2022

---
+++ LabSintaxis2/LabSintaxis2/LabSintaxis4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis4
+{
+    class Program
+    {
+        public void mostrarSuma(int nro1, int nro2)
+        {
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
