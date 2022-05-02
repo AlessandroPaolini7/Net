111b9f5eda6151fbd468ba8401728c8bc9714592
Alessandro <alepaolinicaar@gmail.com>
2022-05-02 11:45:31 -0300

    Parte Ale

---
+++ TP2/Business.Entities/AlumnoInscripcion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class AlumnoInscripcion : BusinessEntity
+    {
+
+        public string Condicion { get; set; }
+        public int IDAlumno { get; set; }
+        public string IDCurso { get; set; }
+        public string Nota { get; set; }
+
+    }
+}
