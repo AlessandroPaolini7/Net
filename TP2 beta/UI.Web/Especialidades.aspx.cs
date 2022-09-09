584825031d02dee8cb2f5516a757f3784e6fdb68
Bautista Guerra <bauguerra@gmail.com>
2022-09-09 19:17:44 -0300

    abm usuario terminado

---
+++ TP2 beta/UI.Web/Especialidades.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Web
+{
+    public partial class Especialidades : formApplication
+    {
+        protected new void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack) this.LoadGrid();
+        }
+        public FormModes FormMode
+        {
+            get
+            {
+                return (FormModes)this.ViewState["FormMode"];
+            }
+            set
+            {
+                this.ViewState["FormMode"] = value;
+            }
+        }
+
+        protected int SelectedID
+        {
+            get
+            {
+                if (this.ViewState["SelectedID"] != null) return (int)this.ViewState["SelectedID"];
+                else return 0;
+            }
+            set
+            {
+                this.ViewState["SelectedID"] = value;
+            }
+        }
+        protected bool IsEntitySelected
+        {
+            get { return (this.SelectedID != 0); }
+        }
+
+        EspecialidadLogic _Esplogic;
+        private EspecialidadLogic Esplogic
+        {
+            get
+            {
+                if (_Esplogic == null)
+                {
+                    _Esplogic = new EspecialidadLogic();
+                }
+                return _Esplogic;
+            }
+        }
+
+
+
+        private Especialidad Entity { get; set; }
+
+        private void LoadGrid()
+        {
+            this.gridView.DataSource = this.Esplogic.GetAll();
+            gridView.DataBind();
+        }
+
+        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.gridView.SelectedValue;
+        }
+
+        private void LoadForm(int id)
+        {
+            this.Entity = this.Esplogic.GetOne(id);
+            this.descripcionTextBox.Text = this.Entity.Descripcion;
+        }
+
+        protected void editarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Modificacion;
+                this.EnableForm(true);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        private void LoadEntity(Especialidad especialidad)
+        {
+            especialidad.Descripcion = this.descripcionTextBox.Text;
+        }
+
+        private void SaveEntity(Especialidad especialidad)
+        {
+            this.Esplogic.Save(especialidad);
+        }
+
+        protected void aceptarLinkButton_Click(object sender, EventArgs e)
+        {
+            switch (this.FormMode)
+            {
+                case FormModes.Baja:
+                    this.DeleteEntity(this.SelectedID);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Modificacion:
+                    this.Entity = new Especialidad();
+                    this.Entity.IDEspecialidad = this.SelectedID;
+                    this.Entity.State = BusinessEntity.States.Modified;
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Alta:
+                    this.Entity = new Especialidad();
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+            }
+
+            this.formPanel.Visible = false;
+        }
+
+        private void EnableForm(bool enable)
+        {
+            this.descripcionTextBox.Enabled = enable;
+        }
+
+        protected void eliminarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Baja;
+                this.EnableForm(false);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        private void DeleteEntity(int id)
+        {
+            this.Esplogic.Delete(id);
+        }
+
+        protected void nuevoLinkButton_Click(object sender, EventArgs e)
+        {
+            this.formPanel.Visible = true;
+            this.FormMode = FormModes.Alta;
+            this.ClearForm();
+            this.EnableForm(true);
+        }
+
+        private void ClearForm()
+        {
+            this.descripcionTextBox.Text = string.Empty;
+        }
+    }
+}
